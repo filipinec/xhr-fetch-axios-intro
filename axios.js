3257ee0be90976af3b3c14ec6d25f6a7ef9a91f9
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-02-06 18:40:57 +0100

    AXIOS Method

--- axios.js
+++ axios.js
@@ -1,9 +1,23 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
-const getData = () => {};
+const getData = () => {
+    axios.get('https://reqres.in/api/users/23').then(response => {
+      console.log(response.data)
+    })
+};
 
-const sendData = () => {};
+const sendData = () => {
+  axios.post('https://reqres.in/api/register', {
+    email: "eve.holt@reqres.in",
+    // password: "pistol"
+  }).then(response => {
+    console.log(response);
+  } ).catch(err => {
+    console.log(err, err.response.data)
+
+  })
+};
 
 getBtn.addEventListener('click', getData);
 postBtn.addEventListener('click', sendData);
--- xhr.js
+++ xhr.js
@@ -35,7 +35,7 @@ const getData = () => {
 const sendData = () => {
   sendHttpRequest('POST','https://reqres.in/api/register', {
     email: "eve.holt@reqres.in",
-    // password: "pistol"
+    password: "pistol"
   }).then(responseData =>{
     console.log(responseData);
   }).catch(err => {
